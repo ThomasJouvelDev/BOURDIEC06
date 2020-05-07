d71fb9088b8dff3f2bac8de0f6b02011eab2d86f
thomas jouvel <thomas.jouvel@gmail.com>
2020-05-07 22:17:54 +0200

    fixing posmark

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   config.action_mailer.delivery_method = :postmark
-  config.action_mailer.postmark_settings = { :api_token => Rails.application.secrets.postmark_api_token }
+  config.action_mailer.postmark_settings = { api_token: ENV['POSTMARK_API_TOKEN'] }
   config.action_mailer.default_url_options = { host: "https://bourdiec06.herokuapp.com/" }
   # Settings specified here will take precedence over those in config/application.rb.
 
